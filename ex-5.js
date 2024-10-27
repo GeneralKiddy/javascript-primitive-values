c38caabeca16053653ecd6a08d531a23adb305de
Art <artkt@hotmail.com>
2024-10-27 16:21:22 +0700

    fix: do exercises

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let productPrice = 27000;
+let discount = productPrice * 0.25;
+let priceAfterDiscount = productPrice - discount;
+console.log(priceAfterDiscount);
